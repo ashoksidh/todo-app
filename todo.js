5f0e4df51301cb9e24e5ab87e714f8a54d5035a1
ashoksidh <87526123+ashoksidh@users.noreply.github.com>
2022-06-02 17:42:22 +0530

    todo-app

--- todo.js
+++ todo.js
@@ -2,24 +2,26 @@ let myForm =document.getElementById("myForm");
 let input =document.getElementById("input");
 let myList =document.getElementById("myList");
 let h2 =document.getElementById("h2");
-// let checkbox =document.getElementById("checkbox");
 let taskBox = document.querySelector(".ul");
 
 
 myForm.addEventListener("submit", (data) => {
                   data.preventDefault()
-                  createItem(input.value)
-                  
-              }
-)
+                  createItem(input.value)});
 
 createItem = (d) =>{
-    let myTemplate = `<li class="list" class="line" id="list" onclick=""><div  class="list-items"><input id="myCheckBox" type="checkbox" class="checkbox" 
-     > <span id="inputValue" > ${d}</span></input></div><button onclick="deleteItem(this)" class="deletebtn"><i class="fa fa-trash"></i></button></ li>`
+    let myTemplate = `<li class="list" class="line" id="list" onclick=""><div  class="list-items">
+    <input id="myCheckBox" type="checkbox" class="checkbox" 
+     ></input><span> ${d}</span></div><button onclick="deleteItem(this)" class="deletebtn"><i class="fa fa-trash"></i></button></ li>`
     myList.insertAdjacentHTML("beforeend" , myTemplate)
     input.value = "";
     input.focus();
+	if(myList){
     h2.style.display = 'none';
+	}else 
+	{
+		h2.style.display = 'block';
+	}
     
 
    
@@ -35,18 +37,12 @@ var list = document.querySelector('ul');
 
 
 
-list.addEventListener("click", function(ev) {
-  console.log(ev.target.querySelector("list"))
-  if (ev.target.tagName === 'LI') {          
-    ev.target.classList.toggle('checked');  
+list.addEventListener("click", function(event) {
+  let element = event.target.nextElementSibling;
+  
+  if (element?.classList.contains('checked')) { 
+    element?.classList.remove('checked');
+  }else{
+    element?.classList.add('checked');
   }
-},false);
-
-
-
-
-
-
-
-
-
+}, false);
